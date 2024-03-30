b47e167fd8e2ae72d2994c1fd2ce938fb629be8d
Hardik Agarwal <115066977+hardikagarwal26@users.noreply.github.com>
2024-03-30 21:24:30 +0530

    Time: 2 ms (92.72%), Space: 8.5 MB (97.38%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) {
+            if(!isalnum(s[left])) left++; // Ignore if not alphanumeric
+            else if(!isalnum(s[right])) right--; // Ignore if not alphanumeric
+            else if(tolower(s[left]) != tolower(s[right])) return false;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
