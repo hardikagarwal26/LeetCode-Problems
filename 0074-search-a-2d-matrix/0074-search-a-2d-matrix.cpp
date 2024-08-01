19d41fb2249a6c9718c92a9e24fcb1fba203e13b
Hardik Agarwal <115066977+hardikagarwal26@users.noreply.github.com>
2024-08-02 00:33:47 +0530

    Time: 7 ms (22.26%), Space: 12.2 MB (26.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    // bool bs(vector<int>matrix , int target){
-    //     int n=matrix.size();
-    //     int low=0,high=n-1;
-    //     while(low<=high){
-    //         int mid=(low+high)/2;
-    //         if(matrix[mid]==target){
-    //             return true;
-    //         }
-    //         else if(matrix[mid]<mid)    high=mid-1;
-    //         else low=mid+1;
-    //     }
-    //     return false;
-    // }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
