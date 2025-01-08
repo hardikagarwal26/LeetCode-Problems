29f2d7ac58204c72fdde3f4aea8c8911e29765aa
Hardik Agarwal <115066977+hardikagarwal26@users.noreply.github.com>
2025-01-09 00:17:19 +0530

    Time: 0 ms (100%), Space: 19.1 MB (29.04%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -8,8 +8,8 @@ class Solution:
         if head is None or head.next is None:
             return head
         odd = head
-        evhead = head.next
         even = head.next
+        evhead = head.next
         while even and even.next:
             odd.next = odd.next.next
             even.next = even.next.next
